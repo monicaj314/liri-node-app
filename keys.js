e00a04ecfa6cc73cde3f31638d07023317c4de52
Monica Jones <monicaj314@yahoo.com>
2017-04-03 13:40:18 -0500

    added package.json file with dependencies and updated twitter keys to use test account

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 's7DXrvVYhlcYio5fAAMNKMXie',
-  consumer_secret: 'KEDLeCRulNGTrAnkqVORfN2fGyZaui41OKOsVtquifp4vUha8c',
-  access_token_key: '146537938-7pr1HogA79dzggb49R9KMqPAYugf6ddEwtovKlxr',
-  access_token_secret: 'Kotsnt5Y9JYO2lACNdMty5awgYLHdqi5DNJ9FsVmKeELS',
+  consumer_key: 'h9eFQHdzAo7CHsNPtTbp03pwP',
+  consumer_secret: 'KRmrRqh6lcfMXtHSfI6g7acgsXe3EQZi2CYtkXMlU2yLgLOWai',
+  access_token_key: '848949067141058560-jQolTzypwSOBuxUE24XkJHI8l8FGTxz',
+  access_token_secret: 'tlzLoC8RbRXav3l9FU61WZFAcbE2CNoHb66MagnhY4PdI',
 }
